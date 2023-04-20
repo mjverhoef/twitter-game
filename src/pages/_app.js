cbd3121d1c6ea094b6113b915c0e5a5e5a720334
mjverhoef <mjverhoef01@gmail.com>
2023-04-20 12:42:50 +0200

    Got some basic cards centered

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
